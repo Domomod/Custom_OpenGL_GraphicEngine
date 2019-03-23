14c16c5169cd69514da57b9e1c9ba0abe4f6f42b
Dominik Witczak <domionato@gmail.com>
2019-03-23 14:21:26 +0100

    Commented Entity System

--- Source/DataLayer/StorageManagment/EntitySystem.h
+++ Source/DataLayer/StorageManagment/EntitySystem.h
@@ -13,6 +13,17 @@
 #include "MeshToModelMap.h"
 #include "Source/DataLayer/DataTypes/Geometry/MeshGenerator.h"
 
+/*  Initial purpose of entity system was to behave like a smart database of loaded game objects, meshes etc.
+ *  Also to store them in such a manner that it's fast to draw them instanced or not, and to modify them
+ *  without the need to additionaly bake data. Atm this part is fully implemented.
+ *
+ *  It consists of samller classes that do the actual storing, sorting, acquiring.
+ *
+ *  TODO: fix those:
+ *  TODO: acquiring an entity or mesh is not ideal yet as it's an O(log n) op.
+ *  TODO: not baking data requires the user to refer to pointer to a pointer to modify model data.
+ * */
+
 class EntitySystem {
 public:
     EntitySystem();
